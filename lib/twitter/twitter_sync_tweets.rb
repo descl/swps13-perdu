71d126d8908a0cfba743280f3519c360ef2eabd9
Tolomaus <niek.bartholomeus@gmail.com>
2013-02-09 18:32:32 +0100

    Sync tweets almost finished

--- lib/twitter/twitter_sync_tweets.rb
+++ lib/twitter/twitter_sync_tweets.rb
@@ -1,4 +1,5 @@
 require 'httpclient'
+require 'rest_client'
 require 'json'
 require 'pp'
 
@@ -10,33 +11,53 @@ class TwitterSyncTweets
 
   def launch
     http = HTTPClient.new
-    file=File.open("max_twitter_id.txt", 'r')
-    max_twitter_id=file.read.to_i
-    file.close
+
+    file_name="#{File.dirname(__FILE__)}/max_twitter_id.txt"
+
+    max_twitter_id=0
+
+    File.open(file_name, 'r') {|file| max_twitter_id=file.read.to_i}
 
     while true do
-      puts "Starting sync ..."
-      response = http.get "http://search.twitter.com/search.json?q=%23loooze&include_entities=true&since_id=#{max_twitter_id}"
+      search_url= "http://search.twitter.com/search.json?q=%23loooze&include_entities=true&since_id=#{max_twitter_id}"
+      puts "Starting sync ... (url: #{search_url})"
+
+      response = http.get search_url
       tweets = JSON.parse(response.content)["results"]
 
+      tweets.sort_by! {|tweet| tweet["id"].to_i}
+
       tweets.each do |tweet|
-        next if tweet["from_user"]="looozecom"
+        next if tweet["from_user"]=="looozecom"
 
         puts "new tweet: #{tweet["text"]}"
+        pp tweet
 
-        objet=Objets.new
-        objet.text=tweet["text"]
-        objet.save
-        if tweet.id > max_twitter_id
-          max_twitter_id=tweet.id
+        user_name = tweet["from_user"]
+        name = tweet["text"]
+        picture_url = tweet["entities"]["media"][0]["media_url"] if tweet["entities"].has_key?("media")
+
+        puts "posting new tweet to loooze.com ..."
+        begin
+          response=RestClient.post 'http://www.loooze.com/objet/', :name => name, :picture => picture_url #, :user_name => user_name,
+          if response.code >= 300
+            puts "HTTP error occurred - code: #{response.code}"
+            return
+          end
+        rescue Exception => e
+          puts "Unexpected error occurred: #{e.inspect}"
+          return
         end
+        puts "post finished successfully."
+
+        max_twitter_id=tweet["id"]
+
+        File.open(file_name, 'w') {|file| file.write(max_twitter_id) }
       end
-      file=File.open("max_twitter_id.txt", 'w')
-      file.write(max_twitter_id)
-      file.close
 
       puts "sync finished."
 
+      puts "sleep 30 sec ..."
       sleep(30)
     end
   end
